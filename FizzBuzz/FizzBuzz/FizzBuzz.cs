804298b94f1f73399817d84155de4d95678c916a
kripa <kripa.kalyanraman@gmail.com>
2015-09-27 17:47:01 -0400

    Commit1

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -6,9 +6,31 @@
 		{
 			string result = number.ToString();
 
-			// your solution/implementation should go in here
+            // your solution/implementation should go in here
+            if (number > 0)
+            {
 
-			return result;
+                if ((number % 3 == 0) && (number % 5 == 0))
+                {
+                    return "FizzBuzz";
+                }
+                else if (number % 3 == 0)
+                {
+                    return "Fizz";
+                    //     break;
+                }
+                else if (number % 5 == 0)
+                {
+                    return "Buzz";
+                    //       break;
+                }
+                else
+                    return result;
+            }
+            else
+                return result;
+
+          //  return result;
 		}
 	}
 }
--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace FizzBuzz
+﻿using System;
+namespace FizzBuzz
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-		}
+            FizzBuzz f = new FizzBuzz();
+            Console.WriteLine("Enter number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            String value = f.RunFizzBuzz(number);
+            Console.WriteLine(value);
+        }
 	}
 }
