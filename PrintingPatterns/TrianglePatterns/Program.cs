20cf6ac7c085e97f4ccb72b15658f981f501988d
kripa <kripa.kalyanraman@gmail.com>
2015-09-30 20:08:50 -0400

    commit1

--- PrintingPatterns/TrianglePatterns/Program.cs
+++ PrintingPatterns/TrianglePatterns/Program.cs
@@ -16,22 +16,66 @@ namespace TrianglePatterns
 
 		static void DisplayPatternA()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
 
 		static void DisplayPatternB()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            for (int i = 10; i >= 1; i--)
+            {
+                for (int j = i; j >= 1; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
 
 		static void DisplayPatternC()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            int space = 0;
+            for (int i = 10; i >= 1; i--)
+            {
+                for (int k = 0; k < space; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = i; j >= 1; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+                ++space;
+            }
+        }
 
 		static void DisplayPatternD()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            int space = 9;
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int k = space; k > 0; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+                --space;
+            }
+        }
 	}
 }
