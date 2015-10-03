d183590cd3e6b20dc122dec62c2b087e45ec972a
kripa <kripa.kalyanraman@gmail.com>
2015-10-03 18:54:45 -0400

    Revert "final commit"
    This reverts commit 091b006c4f02dfd097607d95bc0d0aca1ab31b22.

---
+++ FindingPrimeNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace FindingPrimeNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            int num;
+            int sum;
+            FindingPrimeNumbers f = new FindingPrimeNumbers();
+            Console.WriteLine("Enter the number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            sum = f.FindSumOfPrimeNumbers(num);
+            Console.WriteLine(sum);
+        }
+	}
+}
+																																																		
