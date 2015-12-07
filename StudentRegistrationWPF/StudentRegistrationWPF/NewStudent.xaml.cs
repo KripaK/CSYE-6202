e23f18c0ff759bcd941fc445420b42ccb2350d86
kripa <kripa.kalyanraman@gmail.com>
2015-12-07 14:41:39 -0500

    commit

---
+++ StudentRegistrationWPF/StudentRegistrationWPF/NewStudent.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudentRegistrationWPF
+{
+    /// <summary>
+    /// Interaction logic for NewStudent.xaml
+    /// </summary>
+    public partial class NewStudent : Window
+    {
+        private List<Student> mockStudentList;
+        string firstName, lastName, nuid, dept, enrollType;
+        public NewStudent(List<Student> mockStudentList)
+        {
+            InitializeComponent();
+            LoadDepartments();
+            LoadDefaults();
+            this.mockStudentList = mockStudentList;
+        }
+
+        private void btnAdd_Click_1(object sender, RoutedEventArgs e)
+        {
+            loadData();
+            StudentRegistrationForm form = new StudentRegistrationForm(mockStudentList);
+            form.Show();
+            this.Close();
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            firstText.Clear();
+            lastText.Clear();
+            studentText.Clear();
+            radioButtonFullTime.Select();
+            comboBoxDepartment.SelectedIndex = 0;
+        }
+
+        private void LoadDepartments()
+        {
+            comboBoxDepartment.Items.AddRange(new[] { "Information Systems", "International Affairs", "Nursing", "Pharmacy",
+                "Professional Studies", "Psychology", "Public Administration" });
+        }
+
+        private void LoadDefaults()
+        {
+            radioButtonFullTime.Select();
+            comboBoxDepartment.SelectedIndex = 0;
+
+        }
+
+        private void loadData()
+        {
+            firstName = "bags";
+            lastName = "hamsa";
+            nuid = "111-233-545";
+            //if(string.IsNullOrEmpty(firstName) || string.IsNullOrWhiteSpace(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrWhiteSpace(lastName) || string.IsNullOrEmpty(nuid) || string.IsNullOrWhiteSpace(nuid))
+            //{
+            //    StudentRegistrationWarning popup = new StudentRegistrationWarning();
+            //    DialogResult dialogresult = popup.ShowDialog();
+            //    if (dialogresult == DialogResult.OK)
+            //    {
+            //        popup.Close();
+            //    }
+            //    popup.Dispose();
+            //}
+            dept = comboBoxDepartment.SelectedItem.ToString();
+            if (radioButtonFullTime.Checked == true)
+            {
+                enrollType = "Full Time";
+            }
+            else
+            {
+                enrollType = "Part Time";
+            }
+            Student student = new Student(nuid, firstName, lastName, dept, enrollType);
+            mockStudentList.Add(student);
+            //mockStudentList = new List<Student>() { (new Student(nuid, firstName, lastName, dept, enrollType)) };
+
+        }
+    }
+}
---
+++ StudentRegistrationWPF/StudentRegistrationWPF/StudentRegistration.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudentRegistrationWPF
+{
+    /// <summary>
+    /// Interaction logic for StudentRegistration.xaml
+    /// </summary>
+    public partial class StudentRegistration : Window
+    {
+        private List<Student> mockStudentList;
+        public StudentRegistration(List<Student> mockStudentList)
+        {
+            InitializeComponent();
+            this.mockStudentList = mockStudentList;
+            Init();
+        }
+
+        private void Init()
+        {
+            LoadDepartments();
+            LoadDataGridWithMockData();
+            LoadDefaults();
+            //HandleDataGridViewStudentsSelectionChanged();
+        }
+
+        private void LoadDepartments()
+        {
+            comboBoxDepartment.Items.AddRange(new[] { "Information Systems", "International Affairs", "Nursing", "Pharmacy",
+                "Professional Studies", "Psychology", "Public Administration" });
+        }
+
+        // since we don't know ADO.NET and/or File I/O we will get static mock data
+        private void LoadDataGridWithMockData()
+        {
+            //mockStudentList = new List<Student>()
+            //{
+            //    new Student("111-11-1212", "Bart", "Simpson", "Information Systems", "Full Time"),
+            //    new Student("123-12-1234", "Maggie", "Simpson", "International Affairs", "Part Time"),
+            //};
+            //mockStudentList.Add(new Student("111-11-1212", "Bart", "Simpson", "Information Systems", "Full Time"));
+            //mockStudentList.Add(new Student("123-12-1234", "Maggie", "Simpson", "International Affairs", "Part Time"));
+
+            // dirty workaround to make sure that we can bind to the static mock list
+            var bindingList = new System.ComponentModel.BindingList<Student>(mockStudentList);
+            var source = new BindingSource(bindingList, null);
+            dataGridViewStudents.DataSource = source;
+        }
+
+        private void LoadDefaults()
+        {
+            radioButtonFullTime.Select();
+            comboBoxDepartment.SelectedIndex = 0;
+
+        }
+
+
+        private void HandleDataGridViewStudentsSelectionChanged(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridViewStudents.SelectedRows)
+            {
+                studentText.Text = row.Cells[0].Value.ToString();
+                firstText.Text = row.Cells[1].Value.ToString();
+                lastText.Text = row.Cells[2].Value.ToString();
+
+                comboBoxDepartment.SelectedIndex = comboBoxDepartment.Items.IndexOf(row.Cells[3].Value.ToString());
+
+                // enrollment type selection driven by the grid itself
+                var enrollmentType = row.Cells[4].Value.ToString();
+                if (radioButtonFullTime.Text == enrollmentType)
+                {
+                    radioButtonFullTime.Checked = true;
+                }
+                else if (radioButtonPartTime.Text == enrollmentType)
+                {
+                    radioButtonPartTime.Checked = true;
+                }
+            }
+        }
+        private void btnNewStudent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRemoveStudent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEditStudent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
