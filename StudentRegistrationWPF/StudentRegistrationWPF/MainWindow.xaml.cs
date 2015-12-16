91f8dccf95e72d95f84e9f213e8707f67064ce7d
kripa <kripa.kalyanraman@gmail.com>
2015-12-16 18:07:34 -0500

    commit

--- StudentRegistrationWPF/StudentRegistrationWPF/MainWindow.xaml.cs
+++ StudentRegistrationWPF/StudentRegistrationWPF/MainWindow.xaml.cs
@@ -20,9 +20,72 @@ namespace StudentRegistrationWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Student> mockStudentList = new List<Student>()
+            {
+                new Student("111-11-1212", "Bart", "Simpson", "Information Systems", "Full Time"),
+                new Student("123-12-1234", "Maggie", "Simpson", "International Affairs", "Part Time"),
+            };
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = textBox.Text;
+            string pass = passwordBox.Password;
+            //Boolean check;
+            int count = 0;
+            MainWindow obj = new MainWindow();
+            try
+            {
+                StudentRegistrationForm form = new StudentRegistrationForm(mockStudentList);
+
+                if (check() == false)
+                {
+                    // obj.Show();
+                    MessageBox.Show("Empty field/s");
+                }
+                else if (string.Compare(text, "demouser") == 0 && string.Compare(pass, "demouser") == 0)
+                {
+                    form.Show();
+                }
+                else if (string.Compare(text, "demouser") != 0 && string.Compare(pass, "demouser") != 0)
+                {
+                    count = count + 1;
+                    if (count > 2)
+                    {
+                        MessageBox.Show("Entered wrong username and password three times");
+                        obj.Close();
+
+                    }
+                    MessageBox.Show("Invalid username or password");
+                    textBox.Clear();
+                    passwordBox.Clear();
+                }
+
+                else
+                {
+                    obj.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Exception");
+
+            }
+        }
+
+        private Boolean check()
+        {
+            if ((string.IsNullOrEmpty(textBox.Text)) || string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
